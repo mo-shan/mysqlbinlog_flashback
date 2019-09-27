f5caf9efac4d9b03d41bded7d4b9ff52e593336e
moshan <_shan@yeah.net>
2019-09-27 11:40:22 +0800

    add test.sh

--- test.sh
+++ test.sh
@@ -7,3 +7,4 @@
 #########################################################################
 python mysqlbinlog_back.py --host=10.8.101.133 --username=test --port=3306 --password=test --schema=yybgrk  --table=${t} -S mysqld-bin.005087 --binlog_start_time='2019-09-26 21:18:11' --binlog_end_time='2019-09-26 23:58:27' --skip_insert --skip_update -l tmp_sql
 python mysqlbinlog_back.py --host=172.28.85.43  --username=test --port=3311 --password=test --schema=mljr  --table=moshan -S mysql-bin.000036 --binlog_start_time='2019-09-27 11:31:40' --skip_insert --skip_update -l tmp_sql
+python mysqlbinlog_back.py --host=172.28.85.43  --username=test --port=3311 --password=test --schema=mljr  --table=moshan,t_1 -S mysql-bin.000036 --binlog_start_time='2019-09-27 11:31:40' --skip_insert --skip_update -l tmp_sql #多表用逗号隔开
